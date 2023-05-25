4ed62adaf193c2c9ac932df399420acea722415d
Amy <amyl6086@gmail.com>
2023-05-25 22:14:18 +0100

    remove punctuation

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,7 @@
 export const Quote = ({ quote, author, context }) => {
     return (
         <li className="quote-item">
-            <p className="quote">"{quote}".</p>
+            <p className="quote">{quote}</p>
             <h3 className="author">{author}</h3>
             <span className="context">{context}</span>
         </li>
